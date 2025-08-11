e7976f4c00760617f8d61fd72b1b884a85230953
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2025-08-10 20:02:01 -0400

    Attempt callum on Atreus

---
+++ keyboards/keyboardio/atreus/keymaps/callum/keymap.c
@@ -0,0 +1,7 @@
+// Copyright (C) 2019, 2020  Keyboard.io, Inc
+//
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include QMK_KEYBOARD_H
+
---
+++ users/callum/callum.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define LAYOUT_callum(K_01, K_02, K_03, K_04, K_05, K_06, K_07, K_08, K_09, K_10, K_11, K_12, K_13, K_14, K_15, K_16, K_17, K_18, K_19, K_20, K_21, K_22, K_23, K_24, K_25, K_26, K_27, K_28, K_29, K_30, K_31, K_32, K_33, K_34) \
+LAYOUT( \
+    K_01,  K_02,  K_03,  K_04,  K_05,                  K_06,  K_07,  K_08,  K_09,  K_10,  \
+    K_11,  K_12,  K_13,  K_14,  K_15,                  K_16,  K_17,  K_18,  K_19,  K_20,  \
+    K_21,  K_22,  K_23,  K_24,  K_25,  KC_NO,   KC_NO, K_26,  K_27,  K_28,  K_29,  K_30,  \
+    KC_NO, KC_NO, KC_NO, KC_NO, K_31,  K_32,    K_33,  K_34,  KC_NO, KC_NO, KC_NO, KC_NO  \
+)
