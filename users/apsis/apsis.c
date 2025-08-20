579ab26b9574c3606f8a59aba1fdb1e9b10ee976
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2025-08-20 19:37:04 -0400

    Update apsis.c
    Rename base layer enum value

--- users/apsis/apsis.c
+++ users/apsis/apsis.c
@@ -17,7 +17,7 @@
 #define LA_NAV MO(NAV)
 
 enum layers {
-    DEF,
+    BSE,
     SYM,
     NAV,
     NUM,
@@ -35,7 +35,7 @@ enum keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [DEF] = LAYOUT_thirtyfour(
+    [BSE] = LAYOUT_thirtyfour(
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
         KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
