e6ba312940b0adce870dd8eea9d0a31af8c3f9d1
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2025-08-22 05:06:30 -0400

    Update apsis.c

--- users/apsis/apsis.c
+++ users/apsis/apsis.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BSE] = LAYOUT_thirtyfour(
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,            KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
         KC_A,    KC_R,    KC_S,    KC_T,    KC_D,            KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_QUES,
                                        KC_LSFT, LA_NAV,  LA_SYM,  KC_SPC
     ),
 
