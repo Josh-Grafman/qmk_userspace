322f858d9f066724015f380ca7976a6344ddb0c8
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2025-08-22 05:27:26 -0400

    Update apsis.c

--- users/apsis/apsis.c
+++ users/apsis/apsis.c
@@ -33,7 +33,7 @@ enum keycodes {
     OS_SHFT = SAFE_RANGE,
     OS_CTRL,
     OS_ALT,
-    OS_CMD,
+    OS_GUI,
 
     SW_WIN,  // Switch to next window         (cmd-tab)
 };
@@ -54,21 +54,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYM] = LAYOUT_thirtyfour(
         KC_ESC,  KC_LBRC, KC_LCBR, KC_LPRN, KC_TILD,         KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
-        KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,          KC_HASH, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
+        KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,          KC_HASH, OS_GUI,  OS_ALT,  OS_CTRL, OS_SHFT,
         KC_PLUS, KC_PIPE, KC_AT,   KC_BSLS, KC_PERC,         XXXXXXX, KC_AMPR, KC_SCLN, KC_COLN, KC_EXLM,
                                        _______, _______, _______, _______
     ),
 
     [NAV] = LAYOUT_thirtyfour(
         KC_VOLD, SW_WIN,  TABL,    TABR,    KC_VOLU,         QK_BOOT, HOME,    KC_UP,   END,     KC_DEL,
-        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_NO,           KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
+        OS_SHFT, OS_CTRL, OS_ALT,  OS_GUI,  KC_NO,           KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
         UNDO,    CUT,     COPY,    PASTE,   KC_MPLY,         XXXXXXX, KC_PGDN, KC_PGUP, KC_NO,   KC_ENT,
                                        _______, _______, _______, _______
     ),
 
     [NUM] = LAYOUT_thirtyfour(
         KC_7,    KC_5,    KC_3,    KC_1,    KC_9,            KC_8,    KC_0,    KC_2,    KC_4,    KC_6,
-        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,          KC_F10,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
+        OS_SHFT, OS_CTRL, OS_ALT,  OS_GUI,  KC_F11,          KC_F10,  OS_GUI,  OS_ALT,  OS_CTRL, OS_SHFT,
         KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,           KC_F8,   KC_F12,  KC_F2,   KC_F4,   KC_F6,
                                        _______, _______, _______, _______
     ),
@@ -92,7 +92,7 @@ bool is_oneshot_ignored_key(uint16_t keycode) {
     case OS_SHFT:
     case OS_CTRL:
     case OS_ALT:
-    case OS_CMD:
+    case OS_GUI:
         return true;
     default:
         return false;
@@ -100,12 +100,11 @@ bool is_oneshot_ignored_key(uint16_t keycode) {
 }
 
 bool sw_win_active = false;
-bool sw_lang_active = false;
 
 oneshot_state os_shft_state = os_up_unqueued;
 oneshot_state os_ctrl_state = os_up_unqueued;
 oneshot_state os_alt_state = os_up_unqueued;
-oneshot_state os_cmd_state = os_up_unqueued;
+oneshot_state os_gui_state = os_up_unqueued;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     update_swapper(
@@ -126,7 +125,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         keycode, record
     );
     update_oneshot(
-        &os_cmd_state, KC_LCMD, OS_CMD,
+        &os_gui_state, KC_LCMD, OS_GUI,
         keycode, record
     );
 
