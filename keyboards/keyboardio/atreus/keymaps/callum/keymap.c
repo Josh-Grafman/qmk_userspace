1bab35ead0061e46e781e8f6f01944f83c97d1b5
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2025-08-26 19:25:28 -0400

    Cleanup callum

--- keyboards/keyboardio/atreus/keymaps/callum/keymap.c
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2019, 2020  Keyboard.io, Inc
-//
-// this is the style you want to emulate.
-// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
-
-#include QMK_KEYBOARD_H
-
