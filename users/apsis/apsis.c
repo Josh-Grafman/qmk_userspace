1efdc7aedb59a13e31fb6bac2d5d53ec20bfd89f
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2025-08-25 20:46:02 -0400

    Update apsis.c

--- users/apsis/apsis.c
+++ users/apsis/apsis.c
@@ -27,6 +27,7 @@ enum layers {
     NAV,
     SYM,
     NUM,
+    MSE,
 };
 
 enum keycodes {
@@ -88,6 +89,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_7,    KC_5,    KC_3,    KC_1,    KC_9,            KC_8,    KC_0,    KC_2,    KC_4,    KC_6,
         OS_GUI,  OS_ALT,  OS_SHFT, OS_CTRL, KC_F11,          KC_F10,  OS_CTRL, OS_SHFT, OS_ALT,  OS_GUI,
         KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,           KC_F8,   KC_F12,  KC_F2,   KC_F4,   KC_F6,
+                                       _______, _______, _______, _______
+    ),
+    
+    [MSE] = LAYOUT_thirtyfour(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                        _______, _______, _______, _______
     ),
   };
