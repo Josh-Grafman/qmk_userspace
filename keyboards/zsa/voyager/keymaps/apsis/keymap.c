de98fa5937ca1aa00a32e8ad845d250272b6a533
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2025-08-26 01:16:12 -0400

    Update keymap.c

--- keyboards/zsa/voyager/keymaps/apsis/keymap.c
+++ keyboards/zsa/voyager/keymaps/apsis/keymap.c
@@ -1,2 +1,20 @@
 #include "config.h"
 #include QMK_KEYBOARD_H
+
+bool led_update_user(led_t led_state) {
+    static uint8_t num_state = 0;
+    static uint8_t scrl_state = 0;
+    scrl_state = led_state.scroll_lock;
+    if (num_state != led_state.num_lock) {
+        if (led_state.num_lock) {
+            layer_on(MSE);
+        } else {
+            layer_off(MSE);
+            if (scrl_state) {
+                tap_code(KC_SCRL);
+            }
+        }
+        num_state = led_state.num_lock;
+    }
+    return true;
+}
