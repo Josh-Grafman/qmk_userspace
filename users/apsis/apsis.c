682ad0258d5cfa8607ed8a0580d84c4fe40939a1
Josh Grafman <jgrafman@upsource.solutions>
2026-07-17 14:52:03 -0400

    Patch numlock MSE layer behavior

--- users/apsis/apsis.c
+++ users/apsis/apsis.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, KC_SCRL, MS_BTN1, MS_BTN2,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         UNDO,    CUT,     COPY,    PASTE,   XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                       _______, XXXXXXX, XXXXXXX, XXXXXXX
+                                       KC_NUM,  _______, XXXXXXX, KC_NUM
     ),
   };
 
